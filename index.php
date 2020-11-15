d68122d2611b9ef73a189ba0a62c21e8d42bb77e
851sports <851sports@gmail.com>
2020-11-15 17:30:14 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$rut = $_POST["rut"];
+$enunciado = "C1_{$rut}.pdf";
+
+if(is_file($enunciado)){
+
+    $filename = "Registro_C1.csv";
+    $header = ["rut","hora"];
+    $fyh = date('d/m/Y H:i');
+    $data = [$rut,$fyh];
+
+    if(!is_file($filename)){
+        $fp = fopen($filename, 'w');
+        fputcsv($fp, $header);
+    }
+    else{
+        $fp = fopen($filename, "a");
+    }
+    fputcsv($fp, $data);
+    fclose($fp);
+
+    header("Content-type:application/pdf");
+    header("Content-Disposition: inline;filename=Enunciado_C1.pdf");
+    readfile($enunciado);
+
+//    header("Content-type: application/pdf");
+//    header("Content-Disposition: inline; filename=".$filename);
+//    @readfile($filename);
+}
+else{
+    header("Location: home.php?error=1");
+}
+
+?>
